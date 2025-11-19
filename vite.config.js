ff49ebc1992d61c5d372c0668a2d128d568fa39d
IKnowNoHowPotato <abalos1599@gmail.com>
2025-11-19 12:08:09 +0900

    Add GitHub Actions workflow for Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/KalingAppWeb/" : "/",
+  base: "/KalingAppWeb/",
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
